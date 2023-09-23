6a7e817f8c6c7db04b658faa2961813c0a856f58
Marin Shpata <marin.shpata@kcl.ac.uk>
2023-09-23 18:16:01 +0100

    Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError("Number can't be smaller than 1")
+    list = [2]
+    cnt = 1
+    curNo = 2
+    while cnt < number_of_primes:
+        curNo += 1
+        prime = True
+        for i in range(2,curNo):
+            if curNo % i == 0:
+                prime = False
+        if prime:
+            cnt+=1
+            list.append(curNo)
     return list
